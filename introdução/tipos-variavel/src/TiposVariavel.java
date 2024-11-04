9798fc8daf0151513b02fd461b905981b9f7105e
Lucas Rodrigues Pereira <rodrigues.lrp21@gmail.com>
2024-11-04 05:52:55 -0300

    Anotações Tipo variavel

---
+++ introdução/tipos-variavel/src/TiposVariavel.java
@@ -0,0 +1,13 @@
+public class TiposVariavel {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        final String NOME ="Lucas";
+        final short IDADE = 19;
+        System.out.print(NOME + ' '+ IDADE);
+
+
+
+
+    }
+}
