6580174789fa85779b50b099d91c7f18104ae129
Lucas Rodrigues Pereira <rodrigues.lrp21@gmail.com>
2024-11-05 05:57:48 -0300

    Anotações operadores

---
+++ introdução/operadores/src/Operadores.java
@@ -0,0 +1,8 @@
+
+public class Operadores {
+    public static void main(String[] args)  {
+        boolean var = false;
+        String resultado = var== true ?"UIIII" : "Demais";
+       System.out.print(resultado);
+    }
+}
