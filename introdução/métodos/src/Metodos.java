03fb9e38d564d4a3099149eb1c3fdc0ecab7114c
LucasRP <rodrigues.lrp21@gmail.com>
2024-11-30 13:46:41 -0300

    No linux

---
+++ introdução/métodos/src/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+
+    public static double dividir(int divi, int divisor) throws Exception{
+            return divi/divisor;
+        };
+    
+        public static void main(String[] args) throws Exception {
+    
+            System.out.println(dividir(2, 0));
+            double num = dividir(4, 2);
+
+
+    }
+    
+}
---
+++ introdução/terminalEArgumentos/TerminalEArgumentos.java
@@ -0,0 +1,12 @@
+package introducao.terminalEArgumentos;
+
+public class TerminalEArgumentos {
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+    
+
+    }; 
+}
+
+
+
